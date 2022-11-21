03019648d24754026fd75c0289d98e18e66c1bd0
cantsleep <cant_sleep@naver.com>
2022-11-21 19:50:19 +0900

    usePromise add

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from '../../node_modules/axios/index';
+import usePromise from '../lib/usePromise';
 import NewsItem from './NewsItem';
 
 const NewsListBlock = styled.div`
@@ -16,34 +17,48 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
+// const NewsList = ({ category }) => {
+//   const [articles, setArticles] = useState(null);
+//   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? `` : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=f51a4f28a6a5433693cdfb0af5c5f9b8`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const query = category === 'all' ? `` : `&category=${category}`;
+//         const response = await axios.get(
+//           `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=f51a4f28a6a5433693cdfb0af5c5f9b8`,
+//         );
+//         setArticles(response.data.articles);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//       setLoading(false);
+//     };
+//     fetchData();
+//   }, [category]);
+
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=f51a4f28a6a5433693cdfb0af5c5f9b8`,
+    );
   }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
+
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
 
   return (
     <NewsListBlock>
